2121bbcc1115711118adfb3360f482ff9606a179
Marco Pfeiffer <marco@hauptsache.net>
2017-07-03 11:22:58 +0200

    only use one order by regex

--- src/Tca/Field/SelectRelationField.php
+++ src/Tca/Field/SelectRelationField.php
@@ -15,8 +15,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Typo3Api\Builder\TableBuilder;
 
+
 class SelectRelationField extends TcaField
 {
+    const ORDER_BY_REGEX = '/(\s*)(ORDER BY(.*))?$/i';
+
     protected function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
@@ -51,15 +54,15 @@ class SelectRelationField extends TcaField
             // append sys_language_uid if available
             if (isset($foreignTable['ctrl']['languageField'])) {
                 $languageField = $options['foreign_table'] . '.' . $foreignTable['ctrl']['languageField'];
-                $where = preg_replace('/(\s*)(ORDER BY|$)/i', "\\1AND $languageField IN (0, -1) \\2", $where, 1);
+                $where = preg_replace(self::ORDER_BY_REGEX, "\\1AND $languageField IN (0, -1) \\2", $where, 1);
             }
 
             // append sorting if available
             if (isset($foreignTable['ctrl']['sortby'])) {
                 $sortByField = $options['foreign_table'] . '.' . $foreignTable['ctrl']['sortby'];
-                $where = preg_replace_callback('/(\s*)(?:ORDER BY(.*))?$/i', function ($match) use ($sortByField) {
-                    if ($match[2]) {
-                        return $match[1] . 'ORDER BY' . $match[2] . ', ' . $sortByField;
+                $where = preg_replace_callback(self::ORDER_BY_REGEX, function ($match) use ($sortByField) {
+                    if ($match[3]) {
+                        return $match[1] . 'ORDER BY' . $match[3] . ', ' . $sortByField;
                     }
 
                     return $match[1] . 'ORDER BY ' . $sortByField;
@@ -74,9 +77,9 @@ class SelectRelationField extends TcaField
                 }
 
                 $sortByStr = implode(', ', $sortByDefinitions);
-                $where = preg_replace_callback('/(\s*)(?:ORDER BY(.*))?$/i', function ($match) use ($sortByStr) {
-                    if ($match[2]) {
-                        return $match[1] . 'ORDER BY' . $match[2] . ', ' . $sortByStr;
+                $where = preg_replace_callback(self::ORDER_BY_REGEX, function ($match) use ($sortByStr) {
+                    if ($match[3]) {
+                        return $match[1] . 'ORDER BY' . $match[3] . ', ' . $sortByStr;
                     }
 
                     return $match[1] . 'ORDER BY ' . $sortByStr;
