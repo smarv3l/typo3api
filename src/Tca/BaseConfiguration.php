0ba8ff126fed63977d3b32bbcde146e5da73c5c5
Marco Pfeiffer <marco@hauptsache.net>
2017-10-23 12:38:52 +0200

    some formatting

--- src/Tca/BaseConfiguration.php
+++ src/Tca/BaseConfiguration.php
@@ -37,17 +37,19 @@ class BaseConfiguration implements TcaConfigurationInterface
 
     public function getDbTableDefinitions(string $tableName): array
     {
-        return [$tableName => [
-            "uid int(11) NOT NULL auto_increment",
-            "PRIMARY KEY (uid)",
-            "pid INT(11) NOT NULL DEFAULT '0'",
-            "INDEX pid (pid)", // i'm not sure if every table should have an index on pid
+        return [
+            $tableName => [
+                "uid int(11) NOT NULL auto_increment",
+                "PRIMARY KEY (uid)",
+                "pid INT(11) NOT NULL DEFAULT '0'",
+                "INDEX pid (pid)", // i'm not sure if every table should have an index on pid
 
-            "deleted TINYINT(1) DEFAULT '0' NOT NULL",
-            "tstamp INT(11) DEFAULT '0' NOT NULL",
-            "crdate INT(11) DEFAULT '0' NOT NULL",
-            "cruser_id INT(11) DEFAULT '0' NOT NULL",
-            "origUid INT(11) DEFAULT '0' NOT NULL",
-        ]];
+                "deleted TINYINT(1) DEFAULT '0' NOT NULL",
+                "tstamp INT(11) DEFAULT '0' NOT NULL",
+                "crdate INT(11) DEFAULT '0' NOT NULL",
+                "cruser_id INT(11) DEFAULT '0' NOT NULL",
+                "origUid INT(11) DEFAULT '0' NOT NULL",
+            ]
+        ];
     }
 }
